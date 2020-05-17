924f1f0baf197fd968cf0156b1fa694cb5eaf819
Dylan Avery <davery@managedmethods.com>
2020-05-16 20:59:21 -0600

    Add contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card } from "antd";
+
+export default function History(props) {
+  return (
+    <>
+      <div className="container">
+        <Card title="Contact">
+          Colorado Wiki created by Dylan Avery in Denver, Colorado <br></br>
+          Email: dylanpatrickavery [at] gmail.com
+        </Card>
+      </div>
+    </>
+  );
+}
